3d8a7e6acfa2b3b1edba6ce1b2b698b2548a1709
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2026-09-11 16:54:36 +0300

    finished the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,8 +6,12 @@ export const metadata: Metadata = {
 
 export default function Contact() {
   return (
-    <div>
-      <p>contact page</p>
-    </div>
+    <main>
+      <h3>Have a certain project in mind?</h3>
+      <h6>Feel free to contact me via email!</h6>
+      <p>Email: <a href="mailto:abdelrahmanislam116@email.com">abdelrahmanislam116@gmail.com</a></p>
+      <h6>And make sure to check out my Github page to see my projects in more detail</h6>
+      <p>Github: <a href="https://github.com/Abdur-RahmanIslam2010">https://github.com/Abdur-RahmanIslam2010</a></p>
+    </main>
   );
 }
